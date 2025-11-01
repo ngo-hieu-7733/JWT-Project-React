fdf8746a2686f633857bbe2f11d59b6ca521756a
ngo-hieu-7733 <ngohieu815@gmail.com>
2025-11-01 20:53:36 +0700

    config axios

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,15 @@
+import { useEffect } from 'react'
 import './App.css'
+import axios from './util/axios.customize'
 
 function App() {
-
+  useEffect(() => { 
+    const fetchData = async () => {
+      const res = axios.get(`/v1/api`)
+      console.log(">>> Check res: ", res)
+    }
+    fetchData()
+  }, [])
   return (
     <>
       hello worold      
